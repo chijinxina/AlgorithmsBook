352b834ee3acb0807496ceffdd43b2b71cda1f14
chijinxin <chijinxind@hotmail.com>
2017-09-17 22:00:20 +0800

    算法学习

---
+++ src/LCS1.cpp
@@ -0,0 +1,21 @@
+//
+// Created by chijinxin on 17-9-17.
+//
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+
+int main(){
+    string a,b;
+    int n,m;
+    while (cin>>a>>b)
+    {
+        n=a.length();
+        m=b.length();
+        cout<<n<<" "<<m<<endl;
+    }
+    return 0;
+}
+
