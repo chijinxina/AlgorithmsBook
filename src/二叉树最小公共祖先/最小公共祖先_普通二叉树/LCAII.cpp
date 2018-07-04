d079645bcb841eefcd9492d458f49714f09a9398
chijinxin <chijinxind@hotmail.com>
2018-07-04 09:21:33 +0800

    2018/06/28 add median num in stream_data

---
+++ src/二叉树最小公共祖先/最小公共祖先_普通二叉树/LCAII.cpp
@@ -0,0 +1,17 @@
+//
+// Created by chijinxin on 18-7-4.
+//
+#include <iostream>
+#include <Tree33.hpp>
+using namespace std;
+
+/*
+ * 求普通二叉树的最小公共祖先
+ * 采用广度优先遍历，使用hash表记录每个节点的父节点，
+ * 当我们找到给定的两个节点时，通过hash表回溯找到树根的路径，比较两条路径，求出第一个相同的节点，即为所求的最小公共祖先
+ */
+
+int main()
+{
+    return 0;
+}
