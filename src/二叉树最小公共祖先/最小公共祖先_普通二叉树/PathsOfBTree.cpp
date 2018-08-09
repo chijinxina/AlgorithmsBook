04681fc2b68ee589ace3d2a2c102d38055e276d0
chijinxin <chijinxind@hotmail.com>
2018-08-09 13:04:28 +0800

    2018/06/28 add median num in stream_data

--- src/二叉树最小公共祖先/最小公共祖先_普通二叉树/PathsOfBTree.cpp
+++ src/二叉树最小公共祖先/最小公共祖先_普通二叉树/PathsOfBTree.cpp
@@ -80,6 +80,47 @@ public void findPath(Node root ,int n){
         stack.pop();
     }
 
+    public int minNumberInRotateArray(int [] array)
+    {
+
+        if(array == null)
+            return 0;
+        int i = 0, j = array.length - 1, mid = 0;
+        while(array[i] >= array[j])
+        {
+            if(j-i==1)
+            {
+                mid = j;
+                break;
+            }
+
+            mid = (i+j)/2;
+            //如果array[i]==array[j]==array[mid]
+            //则不能用二分查找，因为不确定array[mid]位于哪个有序子数组
+            //只能用顺序查找
+            if(array[i]==array[j]&&array[j]==array[mid])
+            {
+                //在除了Main()方法中调用其他方法需要new实例之外，其他方法中调用同一个类中的方法直接用：方法名（实参）即可，不需要其他操作
+                return findInOrder(array,i,j);
+            }
+
+            if(array[i]<=array[mid])
+                i=mid;
+            else if(array[mid]<=array[j])
+                j=mid;
+        }
+        return array[mid];
+    }
+    //顺序查找
+    public int findInOrder(int[] a, int in1, int in2)
+    {
+        int k = a[in1];
+        for(int i = in1+1; i < in2; i++)
+            if(a[i]<k)
+                k=a[i];
+        return k;
+    }
+
 
 
 int main()
