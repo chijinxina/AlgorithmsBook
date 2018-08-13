0ac4cc455b9474508594ce8480837e7fd3e7b274
chijinxin <chijinxind@hotmail.com>
2018-08-13 09:42:34 +0800

    2018/08/13 append Serialize Binary Tree

---
+++ src/二叉树的序列化与二叉树的构造/SerializeTree.cpp
@@ -0,0 +1,53 @@
+//
+// Created by chijinxin on 18-8-13.
+//
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Tree33.hpp"
+using namespace std;
+/*
+ * 二叉树的序列化
+ */
+
+void SerializeBTree(TreeNode* root, string &result)
+{
+    if(root==NULL)
+    {
+        result.append("$ ");
+        return;
+    }
+    result.append(to_string(root->val));
+    result.append(" ");
+    SerializeBTree(root->left, result);
+    SerializeBTree(root->right, result);
+}
+
+TreeNode* DeSerializeBTree(istringstream &in)
+{
+    string val;
+    in >> val;
+    if(val == "$")
+    {
+        return NULL;
+    }
+    TreeNode* root = new TreeNode(atoi(val.c_str()));
+    root->left = DeSerializeBTree(in);
+    root->right= DeSerializeBTree(in);
+    return root;
+}
+
+
+int main()
+{
+    Tree t;
+    string result;
+    SerializeBTree(t.root, result);
+    cout<<result<<endl;
+
+    istringstream in(result);
+    TreeNode* root = DeSerializeBTree(in);
+    cout<<root->val<<endl;
+
+    return 0;
+}
